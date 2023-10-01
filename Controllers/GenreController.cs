15e60ca7a4261d13d0030220f7cb0f3769a8a260
tyywrobel <tylerbeitel25@gmail.com>
2023-09-30 20:23:47 -0400

    Add project files.

---
+++ Controllers/GenreController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
+namespace MovieList.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public GenreController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Genre
+        public async Task<IActionResult> Index()
+        {
+              return _context.Genres != null ? 
+                          View(await _context.Genres.ToListAsync()) :
+                          Problem("Entity set 'MovieContext.Genres'  is null.");
+        }
+
+        // GET: Genre/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.GenreId == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // GET: Genre/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genre/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GenreId,Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(genre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        // GET: Genre/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Genre/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("GenreId,Name")] Genre genre)
+        {
+            if (id != genre.GenreId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(genre);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GenreExists(genre.GenreId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        // GET: Genre/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres
+                .FirstOrDefaultAsync(m => m.GenreId == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        // POST: Genre/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Genres == null)
+            {
+                return Problem("Entity set 'MovieContext.Genres'  is null.");
+            }
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre != null)
+            {
+                _context.Genres.Remove(genre);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GenreExists(string id)
+        {
+          return (_context.Genres?.Any(e => e.GenreId == id)).GetValueOrDefault();
+        }
+    }
+}
